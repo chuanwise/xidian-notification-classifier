1673368c82770fa8c9994039b9452c5ee463e14d
Chuanwise <chuanwise@qq.com>
2024-01-04 00:02:42 +0800

    fix(crawler): fix url fetching bugs

--- crawler.py
+++ crawler.py
@@ -1,10 +1,24 @@
+#  Copyright 2024 Chuanwise.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import re
 
 import requests
 
 NOTIFICATION_PATTERN = re.compile("<a href=\"(?P<path>.*?)\" target=\"_blank\">(?P<title>.*?)</a><span class=\"rq\">")
 
-NEXT_PAGE_LINK_PATTERN = re.compile("<a href=\"(?P<path>.*?)\" class=\"Next\">下页</a>")
+NEXT_PAGE_LINK_PATTERN = re.compile("<a href=\"[^\d]*?(?P<num>\d+?)[^\d]*?\"\s+?class=\"Next\">下页</a>")
 
 CS_DEPARTMENT_NOTIFICATION_URL = "https://cs.xidian.edu.cn/tzgg.htm"
 
@@ -26,14 +40,14 @@ def crawl_notifications(num_pages: int) -> list[tuple[str, str]]:
     notifications = []
     url = CS_DEPARTMENT_NOTIFICATION_URL
     for i in range(num_pages):
-        url = CS_DEPARTMENT_URL + _append_notifications_from_single_page(notifications, url)
+        url = CS_DEPARTMENT_URL + "/tzgg/" + str(_append_notifications_from_single_page(notifications, url)) + ".htm"
     return notifications
 
 
-def _append_notifications_from_single_page(notifications: list[tuple[str, str]], url: str) -> str:
+def _append_notifications_from_single_page(notifications: list[tuple[str, str]], url: str) -> int:
     response_content = _get_decoded_content(url)
     notifications += NOTIFICATION_PATTERN.findall(response_content)
-    return "/" + NEXT_PAGE_LINK_PATTERN.findall(response_content)[0]
+    return NEXT_PAGE_LINK_PATTERN.findall(response_content)[0]
 
 
 if __name__ == '__main__':
